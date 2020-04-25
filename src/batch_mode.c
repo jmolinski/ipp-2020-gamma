110b90b656e8c5d2ed9a63e83e930a6a43e59b3f
Jakub Molinski <jakub@molinski.dev>
2020-04-25 23:52:27 +0200

    Fix behavior of m/g/p commands in batch mode

--- src/batch_mode.c
+++ src/batch_mode.c
@@ -11,27 +11,31 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/** @brief Wykonuje zadane polecenie.
- * Poza argumentem @p command identyfikującym typ komendy przyjmuje 3 argumenty.
- * Jeżeli dana komenda przyjmuje mniej niż 3 argumenty, dodatkowe argumenty nie są
- * używane.
+/** @brief Wykonuje gamma_move lub gamma_golden_move.
+ * Weryfikuje poprawność przekazanych argumenów i wykonuje zadaną komendę.
  * @param[in,out] g       – wskaźnik na strukturę przechowującą stan grym
- * @param[in] command     – znak oznaczający typ komendy, (m, g, f, b, q lub p),
- * @param[in] args        – argumenty komendy.
- * @return Kod @p NO_ERROR jeżeli operacja przebiegła poprawnie, @p ENCOUNTERED_EOF
- * jeżeli dane wejściowe kończą się przed napotkaniem znaku nowej linii,
- * @p INVALID_VALUE, jeżeli napotkany zostanie niespodziewany znak.
+ * @param[in] command     – znak oznaczający typ komendy (m lub g),
+ * @param[in] player      – numer gracza,
+ * @param[in] x           – numer kolumny,
+ * @param[in] y           – numer wiersza.
+ * @return Kod @p NO_ERROR jeżeli operacja przebiegła poprawnie, @p INVALID_VALUE,
+ * jeżeli wartość któregoś z parametrów jest nieprawidłowa.
  */
- // TODO
 int run_move_or_golden_move(gamma_t *g, char command, uint32_t player, uint32_t x,
                             uint32_t y) {
-    // TODO error checking
+    // TODO tutaj walidacja powinan  byc osobno i wywwalac ERROR zamiast 0 raczej
+    bool move_performed;
     if (command == 'm') {
-        gamma_move(g, player, x, y);
+        move_performed = gamma_move(g, player, x, y);
     } else {
-        gamma_golden_move(g, player, x, y);
+        move_performed = gamma_golden_move(g, player, x, y);
     }
+    printf("%u\n", (unsigned)move_performed);
+//    if (!move_performed) {  // TODO to do walidacji??
+//        return INVALID_VALUE;
+//    }
     return NO_ERROR;
 }
 
@@ -72,7 +76,7 @@ int run_busy_free_fields_or_golden_possible(gamma_t *g, char command, uint32_t p
  * @return Kod @p NO_ERROR jeżeli operacja przebiegła poprawnie, @p ENCOUNTERED_EOF
  * jeżeli dane wejściowe kończą się przed napotkaniem znaku nowej linii,
  * @p INVALID_VALUE, jeżeli któryś z argumentów jest nieprawidłowy,
- * @p MEMORY_ERROR, jeżeli nastąpi krytyczny błąd alokacji pamięci.
+ * @p MEMORY_ERROR, jeżeli nastąpił krytyczny błąd alokacji pamięci.
  */
 int run_command(gamma_t *g, char command, uint32_t args[3]) {
     if (command == 'm' || command == 'g') {
@@ -84,7 +88,8 @@ int run_command(gamma_t *g, char command, uint32_t args[3]) {
         if (rendered_board == NULL) {
             return MEMORY_ERROR;
         }
-        printf("%s", gamma_board(g));
+        printf("%s", rendered_board);
+        free(rendered_board);
         return NO_ERROR;
     }
 }
